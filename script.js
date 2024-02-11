65db812efb97e0bdfa4506df38d2d3555a1d40ab
Priti Rai <138751304+Pritiprty@users.noreply.github.com>
2024-02-11 12:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.getElementById('submitButton').addEventListener('click', function() {
+  // Get form data
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+  var email = document.getElementById('email').value;
+  
+  // Construct the alert message with spaces
+  var alertMessage = "First Name: " + firstName + " " +
+                     "Last Name: " + lastName + " " +
+                     "Phone Number: " + phoneNumber + " " +
+                     "Email ID: " + email;
+  
+  // Show the alert
+  alert(alertMessage);
+});
